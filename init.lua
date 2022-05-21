e305aec0b4dce2d1969799c21b0f450f4de08ba0
Oula Kuuva <oula.kuuva@iki.fi>
2022-05-21 08:43:56 +0300

    Fix terminal settings

--- init.lua
+++ init.lua
@@ -1,12 +1,16 @@
 vim.g.python3_host_prog = vim.env.HOME .. "/.pyenv/versions/pynvim/bin/python3"
 vim.g.node_host_prog = vim.env.HOME .. "/.local/share/nvm/v16.15.0/bin/neovim-node-host"
 
-vim.g.do_filetype_lua = 1 -- use new filetype.lua
+-- Use bash since fish causes slowdowns
+vim.opt.shell = "/usr/bin/env bash"
 
-require("user.impatient") -- speedy gonzales startup
+-- Use filetype plugin written in LUA
+vim.g.do_filetype_lua = 1
+
 require("user.options")
 require("user.keymaps")
 require("user.plugins")
+require("user.impatient") -- speedy gonzales startup
 require("user.material")
 require("user.colorscheme")
 require("user.cmp")
--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -15,7 +15,7 @@ toggleterm.setup({
   persist_size = true,
   direction = "float",
   close_on_exit = true,
-  shell = vim.o.shell,
+  shell = "/usr/bin/env fish",
   float_opts = {
     border = "curved",
     winblend = 0,
