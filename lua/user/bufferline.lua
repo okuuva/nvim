586dec9154b59d4f3ed776ba99f1265eecca638d
Oula Kuuva <oula.kuuva@iki.fi>
2022-06-05 10:58:03 +0300

    feat(bufferline): enable tab close icon

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -43,7 +43,7 @@ bufferline.setup({
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
-    show_close_icon = false,
+    show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
