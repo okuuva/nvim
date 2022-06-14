a331fc4935d08eb8e53d289c3e75fa36e258830f
Oula Kuuva <oula.kuuva@iki.fi>
2022-06-14 16:13:24 +0300

    feat(alpha): add shortcut for closing the dashboard
    Also edited some of the existinc shortcuts.

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -14,12 +14,13 @@ dashboard.section.header.val = {
 }
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
   dashboard.button("s", "ﮛ  Find sesssion", ":Telescope session-lens search_session<CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("e", "  Edit configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("c", "ﰸ  Close dashboard", ":Bdelete <CR>"),
   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
