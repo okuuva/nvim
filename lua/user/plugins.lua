314184a9d5576f0e403786fbe18a8defb980a636
Oula Kuuva <oula.kuuva@iki.fi>
2022-09-21 18:05:12 +0300

    refactor(jsonls): fetch schemas from schemastore
    No need to maintain the list myself when there's a plugin for fetching
    the schemas straight from the schemastore.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -117,6 +117,7 @@ return packer.startup(function(use)
   use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
   use("https://git.sr.ht/~whynothugo/lsp_lines.nvim") -- cool virtual line diagnostics
   use("folke/lua-dev.nvim") -- easier config / plugin development
+  use("b0o/schemastore.nvim") -- json schemas for jsonls
 
   -- Telescope
   use("nvim-telescope/telescope.nvim")
