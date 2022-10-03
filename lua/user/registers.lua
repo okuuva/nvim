34b5eb06c6e860f789d1d6d6e793b57e8e3abb1e
Oula Kuuva <oula.kuuva@iki.fi>
2022-10-03 17:33:03 +0300

    chore(registers): update registers config
    2.0 started using the standard configuration logic of lua plugins.

--- lua/user/registers.lua
+++ lua/user/registers.lua
@@ -1,20 +1,55 @@
-local settings = {
-  registers_tab_symbol = "·",
-  registers_space_symbol = " ",
-  registers_return_symbol = "⏎",
-  registers_delay = 0,
-  registers_register_key_sleep = 0,
-  registers_show_empty_registers = 1,
-  registers_trim_whitespace = 1,
-  registers_hide_only_whitespace = 0,
-  registers_window_border = "none",
-  registers_window_min_height = 3,
-  registers_window_max_width = 100,
-  registers_show = '*+"-/_=#%.0123456789abcdefghijklmnopqrstuvwxyz:',
-  registers_paste_in_normal_mode = 2,
-  system_clip = 0,
-}
+local registers = require("registers")
+registers.setup({
+  show = '*+"-/_=#%.0123456789abcdefghijklmnopqrstuvwxyz:',
+  show_empty = true,
+  register_user_command = true,
+  system_clipboard = false,
+  trim_whitespace = true,
+  hide_only_whitespace = false,
+  show_register_types = true,
 
-for k, v in pairs(settings) do
-  vim.g[k] = v
-end
+  bind_keys = {
+    normal = registers.show_window({ mode = "motion" }),
+    visual = registers.show_window({ mode = "motion" }),
+    insert = registers.show_window({ mode = "insert" }),
+    registers = registers.apply_register({ delay = 0.1 }),
+    return_key = registers.apply_register(),
+    escape = registers.close_window(),
+    ctrl_n = false,
+    ctrl_p = false,
+    ctrl_j = false,
+    ctrl_k = false,
+  },
+
+  symbols = {
+    newline = "⏎",
+    space = " ",
+    tab = "·",
+    register_type_charwise = "ᶜ",
+    register_type_linewise = "ˡ",
+    register_type_blockwise = "ᵇ",
+  },
+
+  window = {
+    max_width = 100,
+    highlight_cursorline = true,
+    border = "none",
+    transparency = 0,
+  },
+
+  sign_highlights = {
+    cursorline = "Visual",
+    selection = "Constant",
+    default = "Function",
+    unnamed = "Statement",
+    read_only = "Type",
+    expression = "Exception",
+    black_hole = "Error",
+    alternate_buffer = "Operator",
+    last_search = "Tag",
+    delete = "Special",
+    yank = "Delimiter",
+    history = "Number",
+    named = "Todo",
+  },
+})
