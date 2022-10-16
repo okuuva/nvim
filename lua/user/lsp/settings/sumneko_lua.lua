bf399fc30755beaae62950a72f8271bbb38a1201
Oula Kuuva <oula.kuuva@iki.fi>
2022-10-16 10:39:15 +0300

    feat(lsp): add hammerspoon definitions

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -6,12 +6,14 @@ return {
         callSnippet = "Replace"
       },
       diagnostics = {
-        globals = { "vim" },
+        globals = { "vim", "hs" },
       },
       workspace = {
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
           [vim.fn.stdpath("config") .. "/lua"] = true,
+          [vim.fn.expand("$HOME/.hammerspoon")] = true,
+          ['/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/'] = true,
         },
       },
     },
