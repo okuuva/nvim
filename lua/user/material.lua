0d3afb888b82a8d0d36d3446bfb9a2db341b2f86
Oula Kuuva <oula.kuuva@iki.fi>
2022-10-26 20:15:59 +0300

    refactor(material): update settings to match rewrite

--- lua/user/material.lua
+++ lua/user/material.lua
@@ -3,27 +3,55 @@ vim.g.material_style = "darker"
 local colors = require("material.colors")
 local material = require("material")
 material.setup({
-
   contrast = {
+    terminal = false, -- Enable contrast for the built-in terminal
     sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
     floating_windows = false, -- Enable contrast for floating windows
     cursor_line = true, -- Enable darker background for the cursor line
     non_current_windows = true, -- Enable darker background for non-current windows
-    popup_menu = false, -- Enable lighter background for the popup menu
+    filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
   },
 
-  italics = {
-    comments = true, -- Enable italic comments
-    keywords = false, -- Enable italic keywords
-    functions = false, -- Enable italic functions
-    strings = false, -- Enable italic strings
-    variables = false, -- Enable italic variables
+  styles = { -- Give comments style such as bold, italic, underline etc.
+    comments = {
+      italic = true,
+    },
+    strings = {},
+    keywords = {
+      bold = true,
+      italic = true,
+    },
+    functions = {},
+    variables = {},
+    operators = {},
+    types = {},
   },
 
-  contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
-    "terminal", -- Darker terminal background
-    "packer", -- Darker packer background
-    "qf", -- Darker qf list background
+  plugins = { -- Uncomment the plugins that you use to highlight them
+    -- Available plugins:
+    "dap",
+    -- "dashboard",
+    "gitsigns",
+    -- "hop",
+    "indent-blankline",
+    -- "lspsaga",
+    "mini",
+    -- "neogit",
+    "nvim-cmp",
+    -- "nvim-navic",
+    "nvim-tree",
+    -- "sneak",
+    "telescope",
+    "trouble",
+    "which-key",
+  },
+
+  disable = {
+    colored_cursor = true, -- Disable the colored cursor
+    borders = true, -- Disable borders between verticaly split windows
+    background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+    term_colors = false, -- Prevent the theme from setting terminal colors
+    eob_lines = false, -- Hide the end-of-buffer lines
   },
 
   high_visibility = {
@@ -31,17 +59,11 @@ material.setup({
     darker = true, -- Enable higher contrast text for darker style
   },
 
-  disable = {
-    colored_cursor = false, -- Disable the colored cursor
-    borders = true, -- Disable borders between verticaly split windows
-    background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-    term_colors = false, -- Prevent the theme from setting terminal colors
-    eob_lines = false, -- Hide the end-of-buffer lines
-  },
-
   lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
 
-  async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+  async_loading = false, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+  custom_colors = nil, -- If you want to everride the default colors, set this to a function
 
   -- Overwrite highlights with your own
   custom_highlights = {
@@ -59,19 +81,11 @@ material.setup({
     --   reverse = false, -- reverse the fg and bg colors
     --   link = "SomeOtherGroup", -- link to some other highlight group
     -- },
-    VertSplit = { link = "NormalNC" },
-    TSField = { fg = colors.fg },  -- this was set to { fg = colors.text } in upstream which makes sense but I didn't like the look of it
-    NvimTreeSignColumn = { link = "SignColumn" },
-    NvimTreeRootFolder = { fg = colors.accent },
-    TroubleText = { fg = colors.text, bg = colors.none },
-    TroubleCount = { fg = colors.purple, bg = colors.none },
-    TroubleNormal = { fg = colors.fg, bg = colors.none },
-    TroubleSignError = { fg = colors.error, bg = colors.none },
-    TroubleSignWarning = { fg = colors.yellow, bg = colors.none },
-    TroubleSignInformation = { fg = colors.paleblue, bg = colors.none },
-    TroubleSignHint = { fg = colors.purple, bg = colors.none },
-    TroubleFoldIcon = { fg = colors.accent, bg = colors.none },
-    TroubleIndent = { fg = colors.border, bg = colors.none },
-    TroubleLocation = { fg = colors.disabled, bg = colors.none },
+    VertSplit = { bg = colors.editor.bg_alt },
+    WinSeparator = { link = "VertSplit" },
+    ["@variable"] = { link = "Identifier" },
+    ["@field"] = { link = "Identifier" },
+    ["@table_constructor"] = { link = "Identifier" },
+    ["@identifier"] = { link = "Identifier" },
   },
 })
