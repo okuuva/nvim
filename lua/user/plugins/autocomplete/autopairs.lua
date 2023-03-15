1df939499c7866876d548098f4a1f0ec2455a4e1
Oula Kuuva <oula.kuuva@iki.fi>
2023-03-15 21:01:03 +0200

    feat: add nvim-surround

--- lua/user/plugins/autocomplete/autopairs.lua
+++ lua/user/plugins/autocomplete/autopairs.lua
@@ -1,28 +1,35 @@
 return {
-  "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-  config = function()
-    local opts = {
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-        java = false,
-      },
-      disable_filetype = { "TelescopePrompt", "spectre_panel" },
-      fast_wrap = {
-        map = "<C-a>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0, -- Offset from pattern match
-        end_key = "$",
-        keys = "aoeuidhtns-;qjkxbmwvz",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-      },
-    }
-    require("nvim-autopairs").setup(opts)
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-  end,
+  {
+    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+    event = "VeryLazy",
+    config = function()
+      local opts = {
+        check_ts = true,
+        ts_config = {
+          lua = { "string", "source" },
+          javascript = { "string", "template_string" },
+          java = false,
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<C-a>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0, -- Offset from pattern match
+          end_key = "$",
+          keys = "aoeuidhtns-;qjkxbmwvz",
+          check_comma = true,
+          highlight = "PmenuSel",
+          highlight_grey = "LineNr",
+        },
+      }
+      require("nvim-autopairs").setup(opts)
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    config = true,
+  },
 }
--- lua/user/plugins/treesitter/treesitter.lua
+++ lua/user/plugins/treesitter/treesitter.lua
@@ -6,6 +6,7 @@ local M = {
     "nvim-treesitter-context",
     "nvim-treesitter/playground",
     "nvim-treesitter/nvim-treesitter-refactor",
+    "nvim-treesitter/nvim-treesitter-textobjects",
     "JoosepAlviste/nvim-ts-context-commentstring",
     "windwp/nvim-ts-autotag",
     "andymass/vim-matchup",
