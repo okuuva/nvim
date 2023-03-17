9122b7a495ff9151f9f1d8fcf160fc77bce1b0b5
Oula Kuuva <oula.kuuva@iki.fi>
2023-03-17 22:36:54 +0200

    feat: use dpaste.org in paperplanes
    It's easier to delete stuff there if I send something by accident. And
    for that reason there is now keyboard shortcuts for both sending the
    buffer and selection.

--- lua/user/plugins/productivity/paperplanes.lua
+++ lua/user/plugins/productivity/paperplanes.lua
@@ -1,9 +1,29 @@
+local function post_selection()
+  require("paperplanes").post_selection(function(url, err)
+    if url == nil then
+      vim.notify(("paperplanes got no url back from provider: %s"):format(err), vim.log.levels.ERROR)
+      return
+    end
+    local reg = require("user.util").opts("paperplanes.nvim").register or nil
+    local msg_prefix = ""
+    if reg ~= nil then
+      vim.fn.setreg(reg, url)
+      msg_prefix = ('"%s = '):format(reg)
+    end
+    vim.notify(("%s%s"):format(msg_prefix, url), vim.log.levels.INFO)
+  end)
+end
+
 return {
   "rktjmp/paperplanes.nvim",
+  keys = {
+    { "<leader>pp", "<cmd>PP<cr>", desc = "Send buffer to pastebin" },
+    { "<leader>pp", post_selection, mode = "x", desc = "Send selection to pastebin" },
+  },
   cmd = "PP",
   opts = {
     register = "+",
-    provider = "0x0.st",
+    provider = "dpaste.org",
     provider_options = {},
     notifier = vim.notify,
   },
