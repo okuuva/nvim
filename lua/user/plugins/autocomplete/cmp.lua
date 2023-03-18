342832307a05981fd2ef01ef0c013210637f1280
Oula Kuuva <oula.kuuva@iki.fi>
2023-03-18 10:44:19 +0200

    refactor: use lspkind plugin for kind symbols

--- lua/user/plugins/autocomplete/cmp.lua
+++ lua/user/plugins/autocomplete/cmp.lua
@@ -22,42 +22,15 @@ return {
     },
     { "hrsh7th/cmp-nvim-lua", ft = "lua" },
     { "mtoohey31/cmp-fish", ft = "fish" },
+    "onsails/lspkind.nvim", -- fancy icons
     "nvim-autopairs",
   },
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
     --   פּ ﯟ   some other good icons
-    local kind_icons = {
-      Text = "",
-      Method = "m",
-      Function = "",
-      Constructor = "",
-      Field = "",
-      Variable = "",
-      Class = "",
-      Interface = "",
-      Module = "",
-      Property = "",
-      Unit = "",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "",
-      Event = "",
-      Operator = "",
-      TypeParameter = "",
-    }
-    -- find more here: https://www.nerdfonts.com/cheat-sheet
-
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -79,11 +52,16 @@ return {
         ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true, }),
       },
       formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          -- Kind icons
-          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-          vim_item.menu = ({
+        -- fields = { "kind", "abbr", "menu" },
+        format = lspkind.cmp_format({
+
+          -- defines how annotations are shown
+          -- default: symbol
+          -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+          mode = "symbol_text",
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          menu = {
             nvim_lsp = "[LSP]",
             nvim_lua = "[NVIM LUA]",
             luasnip = "[Snippet]",
@@ -92,9 +70,14 @@ return {
             plugins = "[Plugins]",
             buffer = "[Buffer]",
             path = "[Path]",
-          })[entry.source.name]
-          return vim_item
-        end,
+            nerdfont = "[NerdFont]",
+          },
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          -- before = function(entry, vim_item)
+          --   return vim_item
+          -- end,
+        }),
       },
       sources = {
         { name = "nvim_lsp" },
