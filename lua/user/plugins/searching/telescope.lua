07502699eb14599bb7431e875c30ebaabafec95a
Oula Kuuva <oula.kuuva@iki.fi>
2023-03-19 21:18:21 +0200

    feat: use fd by default in telescope.find_files

--- lua/user/plugins/searching/telescope.lua
+++ lua/user/plugins/searching/telescope.lua
@@ -14,7 +14,7 @@ return {
     { "<leader>sb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
     { "<leader>sB", "<cmd>Telescope buffers<cr>", desc = "Open Buffers" },
     { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-    { "<leader>sf", "<cmd>Telescope fd<cr>", desc = "Files" },
+    { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Files" },
     { "<leader>sh", "<cmd>Telescope help_tags theme=ivy<cr>", desc = "Help" },
     { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
     { "<leader>sm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
@@ -125,6 +125,8 @@ return {
         },
       },
       pickers = {
+        find_files = {
+          find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
         },
       },
       extensions = {
