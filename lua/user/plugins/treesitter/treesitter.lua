464f47efa8af95d75c5d410298deedf30e32b3be
Oula Kuuva <oula.kuuva@iki.fi>
2023-03-19 19:38:02 +0200

    refactor: make treesitter plugin spec consistent
    Consistent with other plugin specs that is.

--- lua/user/plugins/treesitter/treesitter.lua
+++ lua/user/plugins/treesitter/treesitter.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
@@ -17,91 +17,89 @@ local M = {
     "windwp/nvim-ts-autotag",
     "andymass/vim-matchup",
   },
-}
-
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      -- usual suspects
-      "diff",
-      "dockerfile",
-      "fish",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "go",
-      "jq",
-      "json",
-      "python",
-      "rst",
-      "sql",
-      "toml",
-      "yaml",
-      -- noice dependencies
-      "bash",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "regex",
-      "vim",
-      -- neoconf dependency
-      "jsonc",
-    }, -- "all", or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    autopairs = { enable = true },
-    autotag = { enable = true },
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "help" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = {
-      enable = true,
-      disable = {
-        "lua",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        -- usual suspects
+        "diff",
+        "dockerfile",
+        "fish",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "jq",
+        "json",
+        "org",
         "python",
+        "rst",
+        "sql",
+        "toml",
         "yaml",
+        -- noice dependencies
+        "bash",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "vim",
+        -- neoconf dependency
+        "jsonc",
+      }, -- "all", or a list of languages
+      sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+      ignore_install = { "" }, -- List of parsers to ignore installing
+      autopairs = { enable = true },
+      autotag = { enable = true },
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "help" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
       },
-    },
-    matchup = { enable = true },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    playground = {
-      enable = true,
-      disable = {},
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = "o",
-        toggle_hl_groups = "i",
-        toggle_injected_languages = "t",
-        toggle_anonymous_nodes = "a",
-        toggle_language_display = "I",
-        focus_language = "f",
-        unfocus_language = "F",
-        update = "R",
-        goto_node = "<cr>",
-        show_help = "?",
-      },
-    },
-    refactor = {
-      highlight_definitions = {
+      indent = {
         enable = true,
-        -- Set to false if you have an `updatetime` of ~100.
-        clear_on_cursor_move = true,
-      },
-      smart_rename = {
-        enable = true,
-        keymaps = {
-          smart_rename = "gr",
+        disable = {
+          "lua",
+          "python",
+          "yaml",
         },
       },
-    },
-  })
-end
-
-return M
+      matchup = { enable = true },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+      refactor = {
+        highlight_definitions = {
+          enable = true,
+          -- Set to false if you have an `updatetime` of ~100.
+          clear_on_cursor_move = true,
+        },
+        smart_rename = {
+          enable = true,
+          keymaps = {
+            smart_rename = "gr",
+          },
+        },
+      },
+    })
+  end,
+}
