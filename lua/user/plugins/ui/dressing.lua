44961a715df032157ae644fbb4ec162caf664007
Oula Kuuva <oula.kuuva@iki.fi>
2023-08-04 16:09:20 +0300

    chore: remove deprecated anchor options from dressing
    These were the defaults anyway so shouldn't affect anything.

--- lua/user/plugins/ui/dressing.lua
+++ lua/user/plugins/ui/dressing.lua
@@ -19,7 +19,6 @@ return {
       start_in_insert = true,
 
       -- These are passed to nvim_open_win
-      anchor = "SW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "cursor",
@@ -121,7 +120,6 @@ return {
       -- Options for built-in selector
       builtin = {
         -- These are passed to nvim_open_win
-        anchor = "NW",
         border = "rounded",
         -- 'editor' and 'win' will default to being centered
         relative = "editor",
