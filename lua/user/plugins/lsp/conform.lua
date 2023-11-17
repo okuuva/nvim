acf4c8a24522fcaca9607b39802b7f8b63c229e1
Oula Kuuva <oula.kuuva@iki.fi>
2023-11-17 16:08:31 +0200

    chore: update conform config

--- lua/user/plugins/lsp/conform.lua
+++ lua/user/plugins/lsp/conform.lua
@@ -1,22 +1,33 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      -- Conform will run multiple formatters sequentially
-      -- Use a sub-list to run only the first available formatter
-      bash = { "shfmt", { "shellharden", "shellcheck" } },
-      fish = { "fish_indent" },
-      go = { { "golines", "goimports", "gofmt" } },
-      javascript = { { "prettierd", "prettier" } },
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      sh = { "shfmt", { "shellharden", "shellcheck" } },
-      ruby = { "rubocop" },
-    },
-    format_on_save = {
-      -- I recommend these options. See :help conform.format for details.
-      lsp_fallback = true,
-    },
-    log_level = vim.log.levels.TRACE, -- ERROR by default, uncomment this for debugging
-  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local opts = {
+      formatters_by_ft = {
+        -- Conform will run multiple formatters sequentially
+        -- Use a sub-list to run only the first available formatter
+        bash = { "shfmt", { "shellharden", "shellcheck" } },
+        fish = { "fish_indent" },
+        go = { { "golines", "goimports", "gofmt" } },
+        javascript = { { "prettierd", "prettier" } },
+        lua = { "stylua" },
+        python = { { "darker", "isort" }, "black" },
+        sh = { "shfmt", { "shellharden", "shellcheck" } },
+        ruby = { "rubocop" },
+      },
+      format_after_save = {
+        lsp_fallback = true,
+      },
+      -- swap between these when debugging
+      log_level = vim.log.levels.ERROR,
+      -- log_level = vim.log.levels.TRACE,
+    }
+    require("conform").setup(opts)
+    require("conform.formatters.black").condition = function()
+      return not require("conform.util").root_file({ ".darker" })
+    end
+    require("conform.formatters.darker").condition = function()
+      return require("conform.util").root_file({ ".darker" })
+    end
+  end,
 }
