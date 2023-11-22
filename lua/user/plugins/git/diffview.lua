b8804bfc79dadb3d398c9abc8b242d00dafd91cc
Oula Kuuva <oula.kuuva@iki.fi>
2023-11-22 21:02:23 +0200

    feat: use <leader>e for toggling files in diffview
    It's the shortcut I use in the main editor as well.

--- lua/user/plugins/git/diffview.lua
+++ lua/user/plugins/git/diffview.lua
@@ -21,5 +21,18 @@ return {
     },
     { "<leader>gi", "<cmd>'<,'>DiffviewFileHistory<cr>", mode = "v", desc = "Line history" },
   },
-  config = true,
+  opts = function()
+    local actions = require("diffview.actions")
+    local common_keymaps = {
+      { "n", "<leader>e", actions.toggle_files, { desc = "Toggle the file panel" } },
+      { "n", "<leader>b", false }, -- disable default toggle files
+    }
+    return {
+      keymaps = {
+        view = common_keymaps,
+        file_panel = common_keymaps,
+        file_history_panel = common_keymaps,
+      },
+    }
+  end,
 }
