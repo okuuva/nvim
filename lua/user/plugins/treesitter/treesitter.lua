6756c58f3731e3a11fb72cee221fac0ce2bbd3f1
Oula Kuuva <oula.kuuva@iki.fi>
2023-11-22 13:20:42 +0200

    chore: bring treesitter config up-to-speed with upstream

--- lua/user/plugins/treesitter/treesitter.lua
+++ lua/user/plugins/treesitter/treesitter.lua
@@ -15,8 +15,16 @@ return {
     "andymass/vim-matchup",
   },
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require("nvim-treesitter.configs").setup({
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
       ensure_installed = {
+        -- mandatory parsers
+        "c",
+        "lua",
+        "query",
+        "vim",
+        "vimdoc",
         -- usual suspects
         "diff",
         "dockerfile",
@@ -36,16 +44,23 @@ return {
         "yaml",
         -- noice dependencies
         "bash",
-        "lua",
         "markdown",
         "markdown_inline",
         "regex",
-        "vim",
         -- neoconf dependency
         "jsonc",
-      }, -- "all", or a list of languages
-      sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-      ignore_install = { "" }, -- List of parsers to ignore installing
+      },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+
+      -- List of parsers to ignore installing (or "all")
+      ignore_install = { "" },
+
       autopairs = { enable = true },
       autotag = { enable = true },
       highlight = {
