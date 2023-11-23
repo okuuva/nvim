a77dda6d71a5cf815995524dc5f1100d01726fc3
Oula Kuuva <oula.kuuva@iki.fi>
2023-11-23 08:06:38 +0200

    feat: add a couple more keymaps for diffview
    Namely toggling word wrap in all diff windows and closing diffview with
    the same keymap I use elsewhere for closing splits.

--- lua/user/plugins/git/diffview.lua
+++ lua/user/plugins/git/diffview.lua
@@ -1,3 +1,22 @@
+local function ToggleWrapInAllDiffViews()
+  -- Save the current window number
+  local current_win = vim.api.nvim_get_current_win()
+
+  -- Iterate over all window IDs
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    -- Set the window as the current window
+    vim.api.nvim_set_current_win(win)
+    -- Check if the window is in diff mode
+    if vim.wo.diff then
+      -- Toggle the 'wrap' option
+      vim.wo.wrap = not vim.wo.wrap
+    end
+  end
+
+  -- Restore the focus to the original window
+  vim.api.nvim_set_current_win(current_win)
+end
+
 return {
   "sindrets/diffview.nvim",
   cmd = {
@@ -28,6 +47,8 @@ return {
   opts = function()
     local actions = require("diffview.actions")
     local common_keymaps = {
+      { "n", "<leader>t", ToggleWrapInAllDiffViews, { desc = "Toggle word wrap" } },
+      { "n", "<leader>C", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
       { "n", "<leader>e", actions.toggle_files, { desc = "Toggle the file panel" } },
       { "n", "<leader>b", false }, -- disable default toggle files
     }
