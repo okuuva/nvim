59d37a043b8d587495f3a647ee0f49fc1901ce9d
Oula Kuuva <oula.kuuva@iki.fi>
2024-01-17 14:52:39 +0200

    fix(lsp): make options optional for custom servers

--- lua/user/plugins/lsp/servers/configs.lua
+++ lua/user/plugins/lsp/servers/configs.lua
@@ -7,8 +7,12 @@ local servers = {
 }
 
 for _, server_name in ipairs(servers) do
-  local opts = require("user.plugins.lsp.servers." .. server_name)
-  M[server_name] = opts
+  local custom_opts_exist, opts = pcall(require, "user.plugins.lsp.servers." .. server_name)
+  if not custom_opts_exist then
+    opts = {}
+  end
+
+  M[server_name] = opts or {}
 end
 
 return M
