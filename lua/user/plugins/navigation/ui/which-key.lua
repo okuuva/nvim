f7c7d8e4ae9d27564997dc35fa0311eb32353939
Oula Kuuva <oula.kuuva@iki.fi>
2024-07-19 11:19:51 +0300

    chore!: update which-key config to v3
    Centering columns isn't a thing anymore. *sniff*

--- lua/user/plugins/navigation/ui/which-key.lua
+++ lua/user/plugins/navigation/ui/which-key.lua
@@ -1,180 +1,72 @@
-local n_leader_mappings = {
-  ["C"] = { "<cmd>close<CR>", "Close split" },
-  ["L"] = { "<cmd>Lazy<cr>", "Lazy" },
-  ["q"] = { "<cmd>qa!<CR>", "Quit" },
-  ["w"] = { "<cmd>w<cr>", "Write" },
-
-  b = { name = "Buffers" },
-
-  d = { name = "Diagnostics" },
-
-  g = {
-    name = "Git",
-    l = { name = "GitLink" },
-    t = { name = "Toggle" },
+---@type wk.Spec
+local mappings = {
+  { "<leader>C", "<cmd>close<CR>", desc = "Close split", nowait = true, remap = false },
+  { "<leader>L", "<cmd>Lazy<cr>", desc = "Lazy", nowait = true, remap = false },
+  { "<leader>N", group = "Neoconf", nowait = true, remap = false },
+  { "<leader>R", group = "IronRepl", nowait = true, remap = false },
+  { "<leader>S", group = "Split", nowait = true, remap = false },
+  { "<leader>SS", "<cmd>vsplit<cr>", desc = "Vertical split", nowait = true, remap = false },
+  { "<leader>Sh", "<cmd>split<cr>", desc = "Horizontal split", nowait = true, remap = false },
+  { "<leader>T", group = "TreeSitter", nowait = true, remap = false },
+  { "<leader>b", group = "Buffers", nowait = true, remap = false },
+  { "<leader>d", group = "Diagnostics", nowait = true, remap = false },
+  { "<leader>g", group = "Git", nowait = true, remap = false },
+  { "<leader>gl", group = "GitLink", nowait = true, remap = false },
+  { "<leader>gt", group = "Toggle", nowait = true, remap = false },
+  { "<leader>h", group = "Harpoon", nowait = true, remap = false },
+  { "<leader>l", group = "LSP", nowait = true, remap = false },
+  { "<leader>m", group = "Markdown Preview", nowait = true, remap = false },
+  { "<leader>n", group = "Neotest", nowait = true, remap = false },
+  { "<leader>nt", group = "Toggle", nowait = true, remap = false },
+  { "<leader>o", group = "Obsidian", nowait = true, remap = false },
+  { "<leader>p", group = "Pastebin", nowait = true, remap = false },
+  { "<leader>q", "<cmd>qa!<CR>", desc = "Quit", nowait = true, remap = false },
+  { "<leader>s", group = "Search", nowait = true, remap = false },
+  { "<leader>w", "<cmd>w<cr>", desc = "Write", nowait = true, remap = false },
+  {
+    mode = { "x" },
+    { "<leader>g", group = "Git" },
+    { "<leader>gl", group = "GitLink" },
+    { "<leader>o", group = "Obsidian" },
+    { "<leader>p", group = "Pastebin" },
+    { "<leader>s", ":<C-u>'<,'>sort<cr>", desc = "Sort" },
   },
-
-  h = { name = "Harpoon" },
-
-  l = { name = "LSP" },
-
-  m = { name = "Markdown Preview" },
-
-  n = { name = "Neotest", t = { name = "Toggle" } },
-
-  N = { name = "Neoconf" },
-
-  o = { name = "Obsidian" },
-
-  p = { name = "Pastebin" },
-
-  R = { name = "IronRepl" },
-
-  s = { name = "Search" },
-
-  S = {
-    name = "Split",
-    h = { "<cmd>split<cr>", "Horizontal split" },
-    S = { "<cmd>vsplit<cr>", "Vertical split" },
-  },
-
-  T = { name = "TreeSitter" },
-}
-
-local x_leader_mappings = {
-  ["s"] = { ":<C-u>'<,'>sort<cr>", "Sort" },
-
-  g = {
-    name = "Git",
-    l = { name = "GitLink" },
-  },
-
-  o = { name = "Obsidian" },
-
-  p = { name = "Pastebin" },
-}
-
-local n_mappings = {
-  g = {
-    l = {
-      name = "Gitlab",
-      a = { name = "Assignee" },
-      r = { name = "Reviewer" },
-    },
-  },
+  { "gl", group = "Gitlab" },
+  { "gla", group = "Assignee" },
+  { "gll", group = "Labels" },
+  { "glr", group = "Reviewer" },
 }
 
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  ---@class wk.Opts
   opts = {
-    plugins = {
-      marks = true, -- shows a list of your marks on ' and `
-      registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-      spelling = {
-        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-        suggestions = 20, -- how many suggestions should be shown in the list?
+    ---@type false | "classic" | "modern" | "helix"
+    preset = "classic",
+    ---@type wk.Win.opts
+    win = {
+      ---@type window_border
+      border = "rounded",
+      title = true,
+      title_pos = "center",
+      -- Additional vim.wo and vim.bo options
+      bo = {},
+      ---@type window_options
+      wo = {
+        winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
       },
-      -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-      -- No actual key bindings are created
-      presets = {
-        operators = true, -- adds help for operators like d, y, ...
-        motions = true, -- adds help for motions
-        text_objects = true, -- help for text objects triggered after entering an operator
-        windows = true, -- default bindings on <c-w>
-        nav = true, -- misc bindings to work with windows
-        z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
-      },
-    },
-    -- add operators that will trigger motion and text object completion
-    -- to enable all native operators, set the preset / operators plugin above
-    operators = { gc = "Comments" },
-    key_labels = {
-      -- override the label used to display some keys. It doesn't effect WK in any other way.
-      -- For example:
-      -- ["<space>"] = "SPC",
-      -- ["<cr>"] = "RET",
-      -- ["<tab>"] = "TAB",
-    },
-    motions = {
-      count = true,
-    },
-    icons = {
-      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-      separator = "➜", -- symbol used between a key and it's label
-      group = "+", -- symbol prepended to a group
-    },
-    popup_mappings = {
-      scroll_down = "<c-d>", -- binding to scroll down inside the popup
-      scroll_up = "<c-u>", -- binding to scroll up inside the popup
-    },
-    window = {
-      border = "shadow", -- none, single, double, shadow
-      position = "bottom", -- bottom, top
-      margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-      padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-      winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
     },
     layout = {
       height = { min = 4, max = 25 }, -- min and max height of the columns
       width = { min = 20, max = 50 }, -- min and max width of the columns
       spacing = 3, -- spacing between columns
-      align = "center", -- align columns left, center or right
     },
-    ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
-    show_help = true, -- show a help message in the command line for using WhichKey
-    show_keys = true, -- show the currently pressed key and its label as a message in the command line
-    triggers = "auto", -- automatically setup triggers
-    -- triggers = {"<leader>"} -- or specify a list manually
-    -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
-    triggers_nowait = {
-      -- marks
-      "`",
-      "'",
-      "g`",
-      "g'",
-      -- registers
-      '"',
-      "<c-r>",
-      -- spelling
-      "z=",
-    },
-    triggers_blacklist = {
-      -- list of mode / prefixes that should never be hooked by WhichKey
-      -- this is mostly relevant for keymaps that start with a native binding
-      i = { "j", "k" },
-      v = { "j", "k" },
-    },
-    -- disable the WhichKey popup for certain buf types and file types.
-    -- Disabled by deafult for Telescope
-    disable = {
-      buftypes = {},
-      filetypes = {},
-    },
+    ---@type wk.Spec
+    spec = mappings,
   },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
-  end,
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
-    wk.register(n_leader_mappings, {
-      mode = "n", -- NORMAL mode
-      prefix = "<leader>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
-    })
-    wk.register(x_leader_mappings, {
-      mode = "x",
-      prefix = "<leader>",
-    })
-    wk.register(n_mappings, {
-      mode = "n",
-    })
   end,
 }
