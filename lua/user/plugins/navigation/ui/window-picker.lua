e6bdf57144979542e2f0899bfc74a4091feab758
Oula Kuuva <oula.kuuva@iki.fi>
2024-09-09 12:33:45 +0300

    feat: update window picker to 2.0

--- lua/user/plugins/navigation/ui/window-picker.lua
+++ lua/user/plugins/navigation/ui/window-picker.lua
@@ -1,6 +1,6 @@
 return {
   "s1n7ax/nvim-window-picker",
-  version = "^1.5.0",
+  version = "^2.0.0",
   keys = {
     {
       "<leader>i",
@@ -12,79 +12,33 @@ return {
     },
   },
   opts = {
-    -- when there is only one window available to pick from, use that window
-    -- without prompting the user to select
-    autoselect_one = true,
-
-    -- whether you want to include the window you are currently on to window
-    -- selection or not
-    include_current_win = false,
+    -- type of hints you want to get
+    -- following types are supported
+    -- 'statusline-winbar' | 'floating-big-letter'
+    -- 'statusline-winbar' draw on 'statusline' if possible, if not 'winbar' will be
+    -- 'floating-big-letter' draw big letter on a floating window
+    -- used
+    hint = "floating-big-letter",
 
     -- when you go to window selection mode, status bar will show one of
     -- following letters on them so you can use that letter to select the window
     selection_chars = "HTNSUEOA",
 
-    -- whether you want to use winbar instead of the statusline
-    -- "always" means to always use winbar,
-    -- "never" means to never use winbar
-    -- "smart" means to use winbar if cmdheight=0 and statusline if cmdheight > 0
-    use_winbar = "never", -- "always" | "never" | "smart"
+    show_prompt = false,
 
-    -- whether to show 'Pick window:' prompt
-    show_prompt = true,
+    filter_func = function(windows, rules)
+      local function predicate(wid)
+        cfg = vim.api.nvim_win_get_config(wid)
+        if not cfg.focusable then
+          return false
+        end
+        return true
+      end
+      local filtered = vim.tbl_filter(predicate, windows)
 
-    -- if you want to manually filter out the windows, pass in a function that
-    -- takes two parameters. you should return window ids that should be
-    -- included in the selection
-    -- EX:-
-    -- function(window_ids, filters)
-    --    -- filter the window_ids
-    --    -- return only the ones you want to include
-    --    return {1000, 1001}
-    -- end
-    filter_func = nil,
-
-    -- following filters are only applied when you are using the default filter
-    -- defined by this plugin. if you pass in a function to "filter_func"
-    -- property, you are on your own
-    filter_rules = {
-      -- filter using buffer options
-      bo = {
-        -- if the file type is one of following, the window will be ignored
-        filetype = { "NvimTree", "neo-tree", "notify" },
-
-        -- if the buffer type is one of following, the window will be ignored
-        buftype = {},
-      },
-
-      -- filter using window options
-      wo = {},
-
-      -- if the file path contains one of following names, the window
-      -- will be ignored
-      file_path_contains = {},
-
-      -- if the file name contains one of following names, the window will be
-      -- ignored
-      file_name_contains = {},
-    },
-
-    -- the foreground (text) color of the picker
-    fg_color = "#ededed",
-
-    -- if you have include_current_win == true, then current_win_hl_color will
-    -- be highlighted using this background color
-    current_win_hl_color = "#e35e4f",
-
-    -- all the windows except the curren window will be highlighted using this
-    -- color
-    other_win_hl_color = "#FF9800",
-
-    -- You can change the display string in status bar.
-    -- It supports '%' printf style. Such as `return char .. ': %f'` to display
-    -- buffer filepath. See :h 'stl' for details.
-    selection_display = function(char)
-      return char
+      local dfilter = require("window-picker.filters.default-window-filter"):new()
+      dfilter:set_config(rules)
+      return dfilter:filter_windows(filtered)
     end,
   },
 }
