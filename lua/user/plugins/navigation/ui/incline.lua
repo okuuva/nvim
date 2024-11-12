b35d3c1ed51776f56e88c1f9b9846a7a184a9c55
Oula Kuuva <oula.kuuva@iki.fi>
2024-11-12 09:11:08 +0200

    feat(plugin): add incline

---
+++ lua/user/plugins/navigation/ui/incline.lua
@@ -0,0 +1,41 @@
+local render = function(props)
+  local helpers = require("incline.helpers")
+  local devicons = require("nvim-web-devicons")
+  local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  if filename == "" then
+    filename = "[No Name]"
+  end
+  local ft_icon, ft_color = devicons.get_icon_color(filename)
+  local modified = vim.bo[props.buf].modified
+  return {
+    ft_icon and { "", guifg = ft_color } or "",
+    ft_icon and { ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+    " ",
+    { filename, gui = modified and "bold,italic" or "bold" },
+    " ",
+  }
+end
+
+return {
+  "b0o/incline.nvim",
+  opts = {
+    highlight = {
+      groups = {
+        InclineNormal = {
+          default = true,
+          group = "Normal",
+        },
+        InclineNormalNC = {
+          default = true,
+          group = "NormalNC",
+        },
+      },
+    },
+    window = {
+      padding = 0,
+      margin = { horizontal = 0 },
+    },
+    render = render,
+  },
+  event = "VeryLazy",
+}
