3000e1df2dee1cc87668821d7a0f6b680d49d772
Oula Kuuva <oula.kuuva@iki.fi>
2024-12-19 13:50:54 +0200

    fix(deferred-clipboard): use default options

--- lua/user/plugins/utils/utils.lua
+++ lua/user/plugins/utils/utils.lua
@@ -62,8 +62,6 @@ return {
     "EtiamNullam/deferred-clipboard.nvim",
     event = "VeryLazy",
     opts = {
-      fallback = "unnamedplus", -- or your preferred setting for clipboard
-      lazy = true,
     },
   },
 }
