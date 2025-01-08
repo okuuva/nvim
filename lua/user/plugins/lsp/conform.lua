b6b778989c91e01a37f5713a169e30bbf733071a
Oula Kuuva <oula.kuuva@iki.fi>
2025-01-08 09:56:52 +0200

    refactor(conform): make syntax work as it used to

--- lua/user/plugins/lsp/conform.lua
+++ lua/user/plugins/lsp/conform.lua
@@ -1,3 +1,32 @@
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
+end
+
+local function expandFormatters(formatters)
+	return function(bufnr)
+		local result = {}
+		for i = 1, #formatters do
+			local formatter = formatters[i]
+			if type(formatter) == "table" then
+				result[i] = first(bufnr, unpack(formatter))
+			else
+				result[i] = formatter
+			end
+		end
+		return result
+	end
+end
+
 return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -6,13 +35,13 @@ return {
       formatters_by_ft = {
         -- Conform will run multiple formatters sequentially
         -- Use a sub-list to run only the first available formatter
-        bash = { "shfmt", { "shellharden", "shellcheck" } },
+        bash = expandFormatters({ "shfmt", { "shellharden", "shellcheck" } }),
         fish = { "fish_indent" },
-        go = { { "golines", "goimports", "gofmt" } },
-        javascript = { { "prettierd", "prettier" } },
+        go = expandFormatters({ { "goimports", "gofmt" } }),
+        javascript = expandFormatters({ { "prettierd", "prettier" } }),
         lua = { "stylua" },
-        python = { { "darker", "isort" }, "black" },
-        sh = { "shfmt", { "shellharden", "shellcheck" } },
+        python = expandFormatters({ { "darker", "isort" }, "black" }),
+        sh = expandFormatters({ "shfmt", { "shellharden", "shellcheck" } }),
         ruby = { "rubocop" },
       },
       format_after_save = {
