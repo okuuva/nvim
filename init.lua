68079310e04ab7dd7ac5bd9016ff9dd08f9ed678
Oula Kuuva <oula.kuuva@iki.fi>
2025-01-10 11:43:48 +0200

    feat(snacks): enable snacks.debug

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
+-- set up debugging shortcuts before anything else
+-- https://github.com/folke/snacks.nvim/blob/main/docs/debug.md
+
+--Show a notification with a pretty printed dump of the object(s) with lua treesitter highlighting
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+
+--Show a notification with a pretty backtrace
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+
+--Override Neovim's vim.print with Snacks.debug.inspect
+vim.print = _G.dd
+
 require("user.config")
 require("lazy").setup("user.plugins", {
   defaults = {
