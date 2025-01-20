54309080750ecaf636c90c79dd9cb5c473abc72e
Oula Kuuva <oula.kuuva@iki.fi>
2025-01-20 08:09:38 +0200

    feat(plugins): give autopairs another chance
    Moved opts into plugin spec and removed settings that were either
    duplicating default behavior or I didn't have a recollection of why they
    were set in the first place.

--- lua/user/plugins/autocomplete/autopairs.lua
+++ lua/user/plugins/autocomplete/autopairs.lua
@@ -1,32 +1,22 @@
 return {
-  {
-    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-    event = "InsertEnter",
-    cond = false,
-    config = function()
-      local opts = {
-        check_ts = true,
-        ts_config = {
-          lua = { "string", "source" },
-          javascript = { "string", "template_string" },
-          java = false,
-        },
-        disable_filetype = { "TelescopePrompt" },
-        fast_wrap = {
-          map = "<C-a>",
-          chars = { "{", "[", "(", '"', "'" },
-          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-          offset = 0, -- Offset from pattern match
-          end_key = "$",
-          keys = "aoeuidhtns-;qjkxbmwvz",
-          check_comma = true,
-          highlight = "PmenuSel",
-          highlight_grey = "LineNr",
-        },
-      }
-      require("nvim-autopairs").setup(opts)
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-    end,
+  "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+  event = "InsertEnter",
+  dependencies = {
+    "nvim-cmp",
   },
+  opts = {
+    check_ts = true, -- use treesitter
+    ts_config = {
+      lua = { "string" }, -- it will not add a pair on that treesitter node
+      javascript = { "template_string" },
+      java = false, -- don't check treesitter on java
+    },
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
