0fe44955e5a0db841cc4100b235ddedaeb01d025
Oula Kuuva <oula.kuuva@iki.fi>
2025-01-28 22:14:19 +0200

    feat(gitsigns)!: remove gitsigns keybindings
    I mostly just use lazygit for everything anyway. And I really should
    learn to use the diffview for when I need it.

--- lua/user/plugins/git/gitsigns.lua
+++ lua/user/plugins/git/gitsigns.lua
@@ -1,17 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  -- stylua: ignore
-  keys = {
-    -- regulars
-    { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk" },
-    { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer" },
-    { "<leader>ga", function() require("gitsigns").stage_hunk() end, desc = "Add Hunk to Stage" },
-    { "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, desc = "Undo Last Stage Hunk" },
-    -- toggles
-    { "<leader>gtd", function() require("gitsigns").toggle_deleted() end, desc = "Deleted lines" },
-    { "<leader>gtw", function() require("gitsigns").toggle_word_diff() end, desc = "Word diff" },
-  },
   opts = {
     signs = {
       add = { text = "▎" },
@@ -27,6 +16,5 @@ return {
       delay = 0,
       ignore_whitespace = true,
     },
-    show_deleted = false,
   },
 }
