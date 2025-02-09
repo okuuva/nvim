9850c53d2dd93e3b2d157ac87db078ffc28b2aff
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-09 22:20:03 +0200

    fix(keybinds): using leap while deleting
    Setting the default keybinds wasn't working for whatever reason. Using
    Lazy keybinds does, as long as leap isn't lazyloaded.

--- lua/user/plugins/navigation/ui/leap.lua
+++ lua/user/plugins/navigation/ui/leap.lua
@@ -1,37 +1,11 @@
 return {
   "ggandor/leap.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-      { "s", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-    },
+  keys = {
+    { "s", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap forward" },
+    { "S", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap backward" },
+    { "gS", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+  },
+  lazy = false,
   dependencies = { "tpope/vim-repeat" },
-  config = function()
-    require("leap").setup({
-      max_phase_one_targets = nil,
-      highlight_unlabeled_phase_one_targets = false,
-      max_highlighted_traversal_targets = 10,
-      case_sensitive = false,
-      -- Sets of characters that should match each other.
-      -- Obvious candidates are braces and quotes ('([{', ')]}', '`"\'').
-      equivalence_classes = { " \t\r\n" },
-      -- Leaving the appropriate list empty effectively disables "smart" mode,
-      -- and forces auto-jump to be on or off.
-      -- commented these out because I don't understand how this is supposed to work
-      -- safe_labels = { 's', 'f', 'n', 'u', 't', . . . },
-      -- labels = { 's', 'f', 'n', 'j', 'k', . . . },
-      special_keys = {
-        repeat_search = "<enter>",
-        next_phase_one_target = "<enter>",
-        next_target = { "<enter>", ";" },
-        prev_target = { "<tab>", "," },
-        next_group = "<space>",
-        prev_group = "<tab>",
-        multi_accept = "<enter>",
-        multi_revert = "<backspace>",
-      },
-    })
-
-    require("leap").set_default_keymaps()
-  end,
+  opts = {},
 }
