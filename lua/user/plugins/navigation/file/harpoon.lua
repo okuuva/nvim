9be9953e6ea433fc8f8507b9ed667024900a95be
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-09 18:31:33 +0200

    fix(harpoon): make c-; work
    Well, not really. But ghostty can be made to send an escape sequence for
    it, meaning we can use that to make it work.

--- lua/user/plugins/navigation/file/harpoon.lua
+++ lua/user/plugins/navigation/file/harpoon.lua
@@ -15,7 +15,7 @@ return {
     { "<C-j>", function() require("harpoon"):list():select(1) end, desc = "Harpoon 1", },
     { "<C-k>", function() require("harpoon"):list():select(2) end, desc = "Harpoon 2", },
     { "<C-l>", function() require("harpoon"):list():select(3) end, desc = "Harpoon 3", },
-    { "<C-;>", function() require("harpoon"):list():select(4) end, desc = "Harpoon 4", },
+    { "<F15>", function() require("harpoon"):list():select(4) end, desc = "Harpoon 4", }, -- assumes ghostty hack to make ctrl+; bindable
   },
   dependencies = { "plenary.nvim" },
   config = function()
