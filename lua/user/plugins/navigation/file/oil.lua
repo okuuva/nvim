acbce1c281f91829b43b9357e4571184c01e0bc7
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-19 17:41:48 +0200

    fix(oil): disable conflicting keymaps
    Harpoon keymaps are important. These I only used by accident when trying
    to use Harpoon and get confused when it did something else I expected.

--- lua/user/plugins/navigation/file/oil.lua
+++ lua/user/plugins/navigation/file/oil.lua
@@ -16,5 +16,14 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      -- htns jkl;
+      -- disable keymaps that conflict with harpoon by setting them to false
+      ["<C-h>"] = false,
+      ["<C-t>"] = false,
+      ["<C-s>"] = false,
+      ["<C-l>"] = false,
+      ["<C-c>"] = false, -- <leader>c closes buffer, no need for a plugin specific mapping
+    },
   },
 }
