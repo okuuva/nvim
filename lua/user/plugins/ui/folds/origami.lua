f3fea2a6e36dabd1e31f8c33d6795839c3972ee3
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-20 09:41:02 +0200

    fix(folds): don't keep folds across sessions in origami
    It caused all kinds of fold shenanigans. Persisted already saves them so
    this was retundant anyway.

--- lua/user/plugins/ui/folds/origami.lua
+++ lua/user/plugins/ui/folds/origami.lua
@@ -2,6 +2,7 @@ return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
   opts = {
+    keepFoldsAcrossSessions = false, -- this being true sometimes breaks folds
     hOnlyOpensOnFirstColumn = true,
   },
 }
