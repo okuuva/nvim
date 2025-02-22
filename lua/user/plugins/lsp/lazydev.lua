ff30be1312c9f9dc431bc242fc498498240a8d6a
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-22 18:05:36 +0200

    feat(lazydev): load lazy.nvim for words opts or config
    The odds are that I'm in a plugin spec file when those are present in a
    lua file.

--- lua/user/plugins/lsp/lazydev.lua
+++ lua/user/plugins/lsp/lazydev.lua
@@ -21,6 +21,7 @@ return {
       -- Load luvit types when the `vim.uv` word is found
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "snacks.nvim", words = { "Snacks" } },
+      { path = "lazy.nvim", words = { "opts", "config" } },
     },
   },
 }
