9d11eabad1f3aa8aa80c89de5324c9cfef2e8de8
Oula Kuuva <oula.kuuva@iki.fi>
2025-04-20 00:18:03 +0300

    fix(img-clip): make it play ball with obsidian
    Now it sets the assets path correctly regardles what the cwd was when
    nvim launched.

--- lua/user/plugins/utils/img-clip.lua
+++ lua/user/plugins/utils/img-clip.lua
@@ -3,6 +3,11 @@
 -- TODO: check oil integration
 -- https://github.com/HakonHarnes/img-clip.nvim?tab=readme-ov-file#oilnvim
 
+-- check if the current file is in obsidian notes dir
+local function notes_dir_path()
+  return vim.fn.finddir("notes", vim.fn.expand("%") .. ";")
+end
+
 ---@type LazyPluginSpec
 return {
   "HakonHarnes/img-clip.nvim",
@@ -12,9 +17,24 @@ return {
   opts = {
     -- recommended settings
     default = {
+      dir_path = function()
+        -- assets/imgs is the default for obsidian.nvim, using it here for consistency
+        return "assets/imgs/" .. vim.fn.expand("%:t:r")
+      end,
       prompt_for_file_name = false,
       drag_and_drop = {
         insert_mode = true,
+      },
+    },
+    custom = {
+      {
+        trigger = function()
+          -- in obsidian notes
+          return notes_dir_path() ~= ""
+        end,
+        dir_path = function()
+          return notes_dir_path() .. "/../assets/imgs/" .. vim.fn.expand("%:t:r")
+        end,
       },
     },
   },
