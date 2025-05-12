eaf915ca8385dcf44fca70a6d91cc4bc18d16b06
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-12 16:54:12 +0300

    fix(origami): config spec changed in a major version bump

--- lua/user/plugins/ui/folds/origami.lua
+++ lua/user/plugins/ui/folds/origami.lua
@@ -2,7 +2,8 @@ return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
   opts = {
-    keepFoldsAcrossSessions = false, -- this being true sometimes breaks folds
-    hOnlyOpensOnFirstColumn = true,
+    foldkeymaps = {
+      hOnlyOpensOnFirstColumn = true,
+    },
   },
 }
