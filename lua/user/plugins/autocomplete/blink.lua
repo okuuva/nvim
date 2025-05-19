c1dfcd58769ce563a1beada09627041d3a3f1b5f
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-19 15:51:42 +0300

    feat(blink): use enter to jump to the next snippet tabstop
    This way both enter and tab behave nicely when using autocomplete, even
    inside snippets.
    
    Requires some workarounds for LuaSnip though. No idea why this isn't the
    default...

--- lua/user/plugins/autocomplete/blink.lua
+++ lua/user/plugins/autocomplete/blink.lua
@@ -75,7 +75,18 @@ return {
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
-      preset = "enter",
+      preset = "default",
+      ["<CR>"] = {
+        function(cmp)
+          if cmp.get_selected_item() ~= nil then
+            return cmp.accept()
+          elseif cmp.snippet_active({ direction = 1 }) then
+            -- jump to next tabstop if not on the last one
+            return cmp.snippet_forward()
+          end
+        end,
+        "fallback",
+      },
       ["<Tab>"] = {
         function(cmp)
           if cmp.is_menu_visible() then
--- lua/user/plugins/autocomplete/luasnip.lua
+++ lua/user/plugins/autocomplete/luasnip.lua
@@ -1,3 +1,44 @@
+-- see https://github.com/L3MON4D3/LuaSnip/issues/656
+-- https://github.com/L3MON4D3/LuaSnip/issues/747#issuecomment-1406946217
+-- https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1500869758
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyLoad",
+  callback = function(event)
+    if event.data ~= "luasnip" then
+      return
+    end
+    local ls = require("luasnip")
+    vim.api.nvim_create_autocmd("ModeChanged", {
+      group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+        clear = true,
+      }),
+      pattern = { "s:n", "i:*" },
+      desc = "Forget the current snippet when leaving the insert mode",
+      callback = function(ev)
+        if not ls.session or not ls.session.current_nodes[ev.buf] or ls.session.jump_active then
+          return
+        end
+
+        local current_node = ls.session.current_nodes[ev.buf]
+        local current_start, current_end = current_node:get_buf_position()
+        current_start[1] = current_start[1] + 1 -- (1, 0) indexed
+        current_end[1] = current_end[1] + 1 -- (1, 0) indexed
+        local cursor = vim.api.nvim_win_get_cursor(0)
+
+        if
+          cursor[1] < current_start[1]
+          or cursor[1] > current_end[1]
+          or cursor[2] < current_start[2]
+          or cursor[2] > current_end[2]
+        then
+          ls.unlink_current()
+        end
+      end,
+    })
+    return true -- disable autocommand after luasnip is loaded
+  end,
+})
+
 ---@type LazyPluginSpec
 return {
   "L3MON4D3/LuaSnip",
