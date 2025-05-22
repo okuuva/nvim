2c6257b3bca2b5271d5f047dd5fddf480536a44d
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-22 10:43:46 +0300

    feat(toggler): use gt for toggling words
    I use tabs so little that gT is more than enough. In addition, I don't
    think I've ever used the go-to type definition shortcut.

--- lua/user/plugins/ide/trouble.lua
+++ lua/user/plugins/ide/trouble.lua
@@ -12,7 +12,6 @@ return {
     { "gd", "<cmd>Trouble lsp_definitions focus=true<cr>", desc = "Goto Definition" },
     { "grr", "<cmd>Trouble lsp_references focus=true<cr>", desc = "Goto References" },
     { "gri", "<cmd>Trouble lsp_implementations focus=true<cr>", desc = "Goto Implementation" },
-    { "gt", "<cmd>Trouble lsp_type_definitions focus=true<cr>", desc = "Goto Type Definition" },
   },
   opts = {},
 }
