f43d3711382eeff3ae23d8c9bf8bc04114f2e057
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-24 20:40:06 +0300

    feat(neoconf): add basic ruff config
    Have to use my own fork of neoconf.nvim until
    https://github.com/folke/neoconf.nvim/pull/114 is merged.

--- lua/user/plugins/lsp/neoconf.lua
+++ lua/user/plugins/lsp/neoconf.lua
@@ -1,5 +1,9 @@
+---@type LazyPluginSpec
 return {
-  "folke/neoconf.nvim",
+  -- TODO: switch back after https://github.com/folke/neoconf.nvim/pull/114 is merged
+  -- "folke/neoconf.nvim",
+  "okuuva/neoconf.nvim",
+  branch = "rename-ruff_lsp-to-ruff",
   cmd = "Neoconf",
   keys = {
     { "<leader>NN", "<cmd>Neoconf<cr>", desc = "Edit local or global config" },
