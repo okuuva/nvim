2a551c1d4f7bf36432f80cc2892b0644f66f5df0
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-25 21:00:43 +0300

    feat(autopairs): add trailing comma to nested tables in lua
    I can't possibly describe how much I love this feature.

--- lua/user/plugins/autocomplete/autopairs.lua
+++ lua/user/plugins/autocomplete/autopairs.lua
@@ -1,3 +1,4 @@
+---@type LazyPluginSpec
 return {
   "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
   event = "InsertEnter",
@@ -9,4 +10,17 @@ return {
       java = false, -- don't check treesitter on java
     },
   },
+  config = function(_, opts)
+    local npairs = require("nvim-autopairs")
+    npairs.setup(opts)
+
+    -- add trailing comma to nested tables
+    local ts_conds = require("nvim-autopairs.ts-conds")
+    local Rule = require("nvim-autopairs.rule")
+    npairs.add_rules({
+      Rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+      Rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+      Rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    })
+  end,
 }
