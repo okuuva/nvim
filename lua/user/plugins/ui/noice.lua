24f4f8b104b4bd7259c5c527202e8f9c9d546d84
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-25 11:33:54 +0300

    feat(noice): use popup for cmdline
    Also set it to use an input dialog for inc-rename and pinned the
    version.

--- lua/user/plugins/ui/noice.lua
+++ lua/user/plugins/ui/noice.lua
@@ -1,12 +1,14 @@
+---@type LazyPluginSpec
 return {
   "folke/noice.nvim",
+  version = "v4.*",
   event = "VeryLazy",
   dependencies = {
     "nui.nvim",
   },
   opts = {
     cmdline = {
-      view = "cmdline",
+      view = "cmdline_popup",
     },
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -31,7 +33,7 @@ return {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = false, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
