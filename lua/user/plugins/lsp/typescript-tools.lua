a016ac1390784ef67412e43eee9d27672143d014
Oula Kuuva <oula.kuuva@iki.fi>
2025-05-29 20:17:57 +0300

    refactor(lsp): replace ts_ls with typescript-tools

---
+++ lua/user/plugins/lsp/typescript-tools.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {
+    settings = {
+      expose_as_code_action = "all",
+    },
+  },
+}
