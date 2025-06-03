b8d6d0f26afc3bd338c34b3651549a1ce662f25c
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-03 20:58:05 +0300

    chore(todo): remove <leader>sT binding

--- lua/user/plugins/syntax-highlighting/todo-comments.lua
+++ lua/user/plugins/syntax-highlighting/todo-comments.lua
@@ -12,8 +12,8 @@ return {
       "snacks.nvim",
       -- stylua: ignore
       keys = {
+        ---@diagnostic disable-next-line: undefined-field
         { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-        { "<leader>sT", function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
       },
     },
   },
