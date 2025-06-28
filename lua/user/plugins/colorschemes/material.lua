26118b2d3cbbffe9e49446ccb7e49282439f4c68
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-28 12:37:50 +0300

    feat(material): set up readable diff colors

--- lua/user/plugins/colorschemes/material.lua
+++ lua/user/plugins/colorschemes/material.lua
@@ -81,13 +81,34 @@ return {
     -- Custom colors must be a function that takes in the default colors table as
     -- a paramter, and then modifies them.
     -- To see the available colors, see lua/material/colors/init.lua
-    custom_colors = function(colors)
-      colors.git.added = "#5E9926"
-      colors.git.removed = "#bf6069"
-    end,
+    -- custom_colors = function(colors)
+    --   colors.git.added = "#5E9926"
+    --   colors.git.removed = "#bf6069"
+    -- end,
 
     -- Overwrite highlights with your own
     custom_highlights = function(colors)
+      -- TODO: move these to custom colors
+      -- TODO: make them a bit more in line with the theme
+      vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#34462F" })
+      vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#462F2F" })
+      vim.api.nvim_set_hl(0, "DiffChange", { bg = "#2F4146" })
+      vim.api.nvim_set_hl(0, "DiffText", { bg = "#463C2F" })
+
+      vim.api.nvim_set_hl(0, "DiffAdded", { fg = colors[3], bold = true })
+      vim.api.nvim_set_hl(0, "DiffRemoved", { fg = colors[2], bold = true })
+      vim.api.nvim_set_hl(0, "DiffChanged", { fg = colors[4], bold = true })
+
+      vim.api.nvim_set_hl(0, "DiffviewWinSeparator", { fg = colors[9] })
+      vim.api.nvim_set_hl(0, "DiffviewDiffDelete", { fg = colors[9] })
+      vim.api.nvim_set_hl(0, "DiffviewFilePanelSelected", { fg = colors[6] })
+
+      vim.api.nvim_set_hl(0, "DiffviewStatusAdded", { fg = colors[3], bold = true })
+      vim.api.nvim_set_hl(0, "DiffviewStatusUntracked", { fg = colors[8], bold = true })
+      vim.api.nvim_set_hl(0, "DiffviewStatusModified", { fg = colors[4], bold = true })
+      vim.api.nvim_set_hl(0, "DiffviewStatusRenamed", { fg = colors[3], bold = true })
+      vim.api.nvim_set_hl(0, "DiffviewStatusDeleted", { fg = colors[2], bold = true })
+      vim.api.nvim_set_hl(0, "DiffviewStatusIgnored", { fg = colors[9], bold = true })
       return {
         -- YourHighlightGroup = {
         --   fg = "#SOME_COLOR", -- foreground color
