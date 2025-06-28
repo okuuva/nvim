f9f683576a6026c3855d34efa52666e45e880b5b
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-28 12:44:29 +0300

    feat(lsp): add vim-rzip
    Needed for Yarn PnP to work.

--- lua/user/plugins/lsp/typescript-tools.lua
+++ lua/user/plugins/lsp/typescript-tools.lua
@@ -1,7 +1,11 @@
 ---@type LazyPluginSpec
 return {
   "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig",
+    "lbrayner/vim-rzip",
+  },
   opts = {
     settings = {
       expose_as_code_action = "all",
