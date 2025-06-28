99a7741bc121c4f3cdc0871439072e012faaaffc
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-28 12:46:22 +0300

    feat(plugins): add tabby

---
+++ lua/user/plugins/ui/tabby.lua
@@ -0,0 +1,30 @@
+---@module "tabby"
+---@type LazyPluginSpec
+return {
+  "nanozuki/tabby.nvim",
+  event = "VimEnter",
+  ---@type TabbyConfig
+  opts = {
+    preset = "active_wins_at_tail",
+    option = {
+      theme = {
+        fill = "TabLineFill", -- tabline background
+        head = "TabLine", -- head element highlight
+        current_tab = "TabLineSel", -- current tab label highlight
+        tab = "TabLine", -- other tab label highlight
+        win = "TabLine", -- window highlight
+        tail = "TabLine", -- tail element highlight
+      },
+      nerdfont = true, -- whether use nerdfont
+      lualine_theme = nil, -- lualine theme name
+      tab_name = {
+        name_fallback = function(tabid)
+          return tabid
+        end,
+      },
+      buf_name = {
+        mode = "unique", -- or 'relative', 'tail', 'shorten'
+      },
+    },
+  },
+}
