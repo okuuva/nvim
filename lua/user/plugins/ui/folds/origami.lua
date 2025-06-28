146d658ed10ca45e481e00a7af38031184ab83e8
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-28 12:45:47 +0300

    chore(plugins): pin origami to current commit

--- lua/user/plugins/ui/folds/origami.lua
+++ lua/user/plugins/ui/folds/origami.lua
@@ -1,6 +1,8 @@
+---@type LazyPluginSpec
 return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
+  commit = "6965856d2282", -- TODO: check if v2.0 could replace ufo
   opts = {
     foldkeymaps = {
       hOnlyOpensOnFirstColumn = true,
