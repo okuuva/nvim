5ea4ba4031a90e7f3ae913920a8d1c656b2663c2
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-28 12:31:45 +0300

    feat(qmk): make config zmk friendly

--- lua/user/plugins/utils/qmk.lua
+++ lua/user/plugins/utils/qmk.lua
@@ -4,12 +4,22 @@ return {
   dependencies = {
     {
       "codethread/qmk.nvim",
-      event = "BufEnter *keymap.c",
+      event = {
+        "BufEnter *keymap.c",
+        "BufEnter *.keymap",
+      },
       opts = {
+        auto_format_pattern = { "*keymap.c", "*.keymap" },
         -- these values are mandatory for the initial setup even if they're never used because the get overridden by
         -- inline JSON
         name = "placeholder",
-        layout = { "x" },
+        layout = {
+          "x x x x x x _ _ _ x x x x x x",
+          "x x x x x x _ _ _ x x x x x x",
+          "x x x x x x _ _ _ x x x x x x",
+          "_ _ _ _ x x x _ x x x _ _ _ _",
+        },
+        variant = "zmk",
       },
     },
   },
