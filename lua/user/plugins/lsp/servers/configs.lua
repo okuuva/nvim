94bc90009aa8054a9fe48b5e0851dbb474b483bd
Oula Kuuva <oula.kuuva@iki.fi>
2025-07-04 19:23:20 +0300

    feat(lsp): add a way to config lsps using vim.lsp

--- lua/user/plugins/lsp/servers/configs.lua
+++ lua/user/plugins/lsp/servers/configs.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local servers = {
+local legacy_servers = {
   "fuzzy_ruby_ls",
   "jsonls",
   "lua_ls",
@@ -8,13 +8,25 @@ local servers = {
   "nushell",
 }
 
-for _, server_name in ipairs(servers) do
+local servers = {
+  -- "tombi", -- in some ways better than taplo but a bit buggy, so continuing with taplo for now
+}
+
+for _, server_name in ipairs(legacy_servers) do
   local custom_opts_exist, opts = pcall(require, "user.plugins.lsp.servers." .. server_name)
   if not custom_opts_exist then
     opts = {}
   end
 
   M[server_name] = opts or {}
+end
+
+for _, server_name in ipairs(servers) do
+  local custom_opts_exist, opts = pcall(require, "user.plugins.lsp.servers." .. server_name)
+  if custom_opts_exist then
+    vim.lsp.config(server_name, opts)
+  end
+  vim.lsp.enable(server_name)
 end
 
 return M
