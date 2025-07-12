25d49f606251542082b79778ae938e9c14802b2d
Oula Kuuva <oula.kuuva@iki.fi>
2025-07-13 00:19:21 +0300

    feat(keymaps): enable origami for arrowkeys

--- lua/user/plugins/ui/folds/origami.lua
+++ lua/user/plugins/ui/folds/origami.lua
@@ -3,8 +3,14 @@ return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
   commit = "6965856d2282", -- TODO: check if v2.0 could replace ufo
+  -- stylua: ignore
+  keys = {
+    { "<Left>", function() require("origami").h() end },
+    { "<Right>", function() require("origami").l() end }
+  },
   opts = {
     foldkeymaps = {
+      setup = false,
       hOnlyOpensOnFirstColumn = true,
     },
   },
