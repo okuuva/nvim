5cb77824fe6d5e64401f6cd9ed4123bdd560a534
Oula Kuuva <oula.kuuva@iki.fi>
2025-09-07 10:49:37 +0300

    feat(keymaps): go back to opening diagnostics in Trouble by default
    I end up sending the search results to quickfix anyway which opens
    Trouble so I guess that was a good flow for me.

--- lua/user/plugins/ide/trouble.lua
+++ lua/user/plugins/ide/trouble.lua
@@ -3,16 +3,16 @@ return {
   "folke/trouble.nvim",
   cmd = { "TroubleToggle", "Trouble" },
   keys = {
-    -- { "<leader>dd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Document diagnostics" },
-    -- { "<leader>dD", "<cmd>Trouble diagnostics toggle<CR>", desc = "Workspace diagnostics" },
-    -- { "<leader>dl", "<cmd>Trouble lsp toggle<CR>", desc = "LSP" },
-    -- { "<leader>do", "<cmd>Trouble loclist toggle<CR>", desc = "Location list" },
-    -- { "<leader>dq", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix" },
-    -- -- dT opens the todo list, defined in todo-comments.lua
-    -- -- lsp
-    -- { "gd", "<cmd>Trouble lsp_definitions focus=true<cr>", desc = "Goto Definition" },
-    -- { "grr", "<cmd>Trouble lsp_references focus=true<cr>", desc = "Goto References" },
-    -- { "gri", "<cmd>Trouble lsp_implementations focus=true<cr>", desc = "Goto Implementation" },
+    { "<leader>dd", "<cmd>Trouble diagnostics filter.buf=0<CR>", desc = "Document diagnostics" },
+    { "<leader>dD", "<cmd>Trouble diagnostics<CR>", desc = "Workspace diagnostics" },
+    { "<leader>dl", "<cmd>Trouble lsp<CR>", desc = "LSP" },
+    { "<leader>do", "<cmd>Trouble loclist<CR>", desc = "Location list" },
+    { "<leader>dq", "<cmd>Trouble qflist<CR>", desc = "Quickfix" },
+    -- dTs the todo list, defined in todo-comments.lua
+    -- lsp
+    { "gd", "<cmd>Trouble lsp_definitions<cr>", desc = "Goto Definition" },
+    { "grr", "<cmd>Trouble lsp_references<cr>", desc = "Goto References" },
+    { "gri", "<cmd>Trouble lsp_implementations<cr>", desc = "Goto Implementation" },
   },
   opts = {
     focus = true,
