b353c346f7f22552b962b7b6090f03ef4b8560b2
Oula Kuuva <oula.kuuva@iki.fi>
2025-09-07 10:49:37 +0300

    feat(neotest): auto-open and auto-focus when running tests
    - Toggle functions now also focus the opened panel
    - Run now also toggles (and focuses) the summary panel unless another
      Neotest panel is already active.

--- lua/user/plugins/ide/neotest.lua
+++ lua/user/plugins/ide/neotest.lua
@@ -1,16 +1,60 @@
+local function _focus(winid)
+  local win = vim.fn.bufwinid(winid)
+  if win > -1 then
+    vim.api.nvim_set_current_win(win)
+  end
+end
+
+local function focus_output()
+  _focus("Neotest Output Panel")
+end
+
+local function toggle_output()
+  require("neotest").output_panel.toggle()
+  focus_output()
+end
+
+local function focus_summary()
+  _focus("Neotest Summary")
+end
+
+local function toggle_summary()
+  require("neotest").summary.toggle()
+  focus_summary()
+end
+
+local function open_summary()
+  require("neotest").summary.open()
+  focus_summary()
+end
+
+local function unless_active(fn)
+  local ft = vim.api.nvim_get_option_value("filetype", {})
+  if ft:find("neotest-") then
+    return
+  end
+  fn()
+end
+
+local function run(...)
+  require("neotest").run.run(...)
+  unless_active(open_summary)
+end
+
+---@type LazyPluginSpec
 return {
   "nvim-neotest/neotest",
   -- version = "^4.1.3",
   -- stylua: ignore
   keys = {
     -- run
-    { "<leader>td", function() require("neotest").run.run(vim.fn.expand('%:h')) end, desc = "Run in current dir" },
-    { "<leader>tn", function() require("neotest").run.run() end, desc = "Run nearest test" },
-    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand('%')) end, desc = "Run current file" },
-    { "<leader>tp", function() require("neotest").run.run(vim.fn.getcwd()) end, desc = "Run tests for the whole project" },
+    { "<leader>td", function() run(vim.fn.expand('%:h')) end, desc = "Run in current dir" },
+    { "<leader>tn", function() run() end, desc = "Run nearest test" },
+    { "<leader>tf", function() run(vim.fn.expand('%')) end, desc = "Run current file" },
+    { "<leader>tp", function() run(vim.fn.getcwd()) end, desc = "Run tests for the whole project" },
     -- toggle
-    { "<leader>tto", function() require("neotest").output_panel.toggle() end, desc = "Output panel" },
-    { "<leader>tts", function() require("neotest").summary.toggle() end, desc = "Summary" },
+    { "<leader>tto", toggle_output, desc = "Output panel" },
+    { "<leader>tts", toggle_summary, desc = "Summary" },
   },
   dependencies = {
     -- common dependencies
