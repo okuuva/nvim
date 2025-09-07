85e9ca61fe004889223f08761ae5c5d97875f20f
Oula Kuuva <oula.kuuva@iki.fi>
2025-09-07 10:49:37 +0300

    feat(snacks): add a separate shortcut for current filetype
    Usually I want a Lua scratch buffer instead of whatever the current
    filetype happens to be. So makes sense to keep the current shortcut for
    the desired default behavior and add a new one for the edge case.

--- lua/user/plugins/utils/snacks/scratch.lua
+++ lua/user/plugins/utils/snacks/scratch.lua
@@ -3,7 +3,8 @@ return {
   "snacks.nvim",
   -- stylua: ignore
   keys = {
-    { "<leader>.",  function() Snacks.scratch() end,        desc = "Toggle Scratch Buffer" },
+    { "<leader>.",  function() Snacks.scratch({ft = "lua"}) end, desc = "Toggle Scratch Buffer" },
+    { "<leader>SS", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer for current filetype" },
     { "<leader>sB", function() Snacks.scratch.select() end, desc = "Scratch Buffers" },
   },
   ---@type snacks.Config
