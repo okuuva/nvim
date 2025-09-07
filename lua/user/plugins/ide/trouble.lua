edec05a188128a3497c7dc36301bdca44c223e3f
Oula Kuuva <oula.kuuva@iki.fi>
2025-09-07 10:49:37 +0300

    feat(trouble): add symbols toggle

--- lua/user/plugins/ide/trouble.lua
+++ lua/user/plugins/ide/trouble.lua
@@ -6,7 +6,8 @@ return {
     { "<leader>dd", "<cmd>Trouble diagnostics filter.buf=0<CR>", desc = "Document diagnostics" },
     { "<leader>dD", "<cmd>Trouble diagnostics<CR>", desc = "Workspace diagnostics" },
     { "<leader>dl", "<cmd>Trouble lsp<CR>", desc = "LSP" },
-    { "<leader>do", "<cmd>Trouble loclist<CR>", desc = "Location list" },
+    { "<leader>dL", "<cmd>Trouble loclist<CR>", desc = "Location list" },
+    { "<leader>ds", "<cmd>Trouble symbols toggle<CR>", desc = "Symbols list" },
     { "<leader>dq", "<cmd>Trouble qflist<CR>", desc = "Quickfix" },
     -- dTs the todo list, defined in todo-comments.lua
     -- lsp
@@ -14,10 +15,19 @@ return {
     { "grr", "<cmd>Trouble lsp_references<cr>", desc = "Goto References" },
     { "gri", "<cmd>Trouble lsp_implementations<cr>", desc = "Goto Implementation" },
   },
+  ---@module "trouble"
+  ---@type trouble.Config
   opts = {
     focus = true,
     preview = {
       border = "none",
+    },
+    modes = {
+      symbols = {
+        win = {
+          size = 60,
+        },
+      },
     },
   },
 }
