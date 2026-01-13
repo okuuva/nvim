fb1f062fc682dfe34f9b0ce7aafc14883127b69b
Oula Kuuva <oula.kuuva@iki.fi>
2026-01-13 17:22:30 +0200

    fix(plugin): visual selection paste in paperplanes

--- lua/user/plugins/productivity/paperplanes.lua
+++ lua/user/plugins/productivity/paperplanes.lua
@@ -1,30 +1,13 @@
-local register = "+"
-
-local function post_selection()
-  require("paperplanes").post_selection(function(url, err)
-    if url == nil then
-      vim.notify(("paperplanes got no url back from provider: %s"):format(err), vim.log.levels.ERROR)
-      return
-    end
-    local reg = register
-    local msg_prefix = ""
-    if reg ~= nil then
-      vim.fn.setreg(reg, url)
-      msg_prefix = ('"%s = '):format(reg)
-    end
-    vim.notify(("%s%s"):format(msg_prefix, url), vim.log.levels.INFO)
-  end)
-end
-
+---@type LazyPluginSpec
 return {
   "rktjmp/paperplanes.nvim",
   keys = {
     { "<leader>PP", "<cmd>PP<cr>", desc = "Send buffer to pastebin" },
-    { "<leader>PP", post_selection, mode = "x", desc = "Send selection to pastebin" },
+    { "<leader>PP", ":PP<cr>", mode = "x", desc = "Send selection to pastebin" },
   },
   cmd = "PP",
   opts = {
-    register = register,
+    register = "+",
     provider = "dpaste.org",
     provider_options = {},
     notifier = vim.notify,
