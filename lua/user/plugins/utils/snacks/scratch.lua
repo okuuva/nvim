8f969def7b8bea509e544a75c114e650d78eae8b
Oula Kuuva <oula.kuuva@iki.fi>
2026-01-26 13:51:37 +0200

    feat(snacks): add shortcut for markdown scratch buffer

--- lua/user/plugins/utils/snacks/scratch.lua
+++ lua/user/plugins/utils/snacks/scratch.lua
@@ -5,6 +5,7 @@ return {
   keys = {
     { "<leader>.",  function() Snacks.scratch({ft = "lua"}) end, desc = "Toggle Scratch Buffer" },
     { "<leader>SS", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer for current filetype" },
+    { "<leader>SM", function() Snacks.scratch({ft = "markdown"}) end, desc = "Toggle Markdown Scratch Buffer" },
     { "<leader>sB", function() Snacks.scratch.select() end, desc = "Scratch Buffers" },
   },
   ---@type snacks.Config
