0f1635423b0c324a5978625c966b61ccfc9f08c3
Oula Kuuva <oula.kuuva@iki.fi>
2026-04-03 22:40:59 +0300

    chore!: update nvim-treesitter

--- lua/user/plugins/treesitter/treesitter.lua
+++ lua/user/plugins/treesitter/treesitter.lua
@@ -1,7 +1,7 @@
 ---@type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- branch = "main",
+  branch = "main",
   build = ":TSUpdate",
   lazy = false,
   cond = function()
@@ -26,7 +26,7 @@ return {
       opts = {},
     },
   },
-  -- registers mise files so that the injector can add syntaxt highlighting for embedded code blocks
+  -- registers mise files so that the injector can add syntax highlighting for embedded code blocks
   -- see https://mise.jdx.dev/mise-cookbook/neovim.html#code-highlight-for-run-commands
   init = function()
     require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
@@ -35,21 +35,20 @@ return {
       local first_line = vim.api.nvim_buf_get_lines(tonumber(bufnr) or 0, 0, 1, false)[1] or ""
       return string.match(filename, ".*mise.*%.toml$") ~= nil or string.match(first_line, "mise") ~= nil
     end, { force = true, all = false })
-    -- add the jjdescription parser
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.jjdescription = {
-      install_info = {
-        url = "https://github.com/kareigu/tree-sitter-jjdescription",
-        files = { "src/parser.c" },
-        branch = "dev",
-        generate_requires_npm = false, -- Try setting this to false
-        requires_generate_from_grammar = false, -- Try setting this to false
-      },
-    }
   end,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    -- In Neovim 0.12, vim.treesitter.start() is only called by default for
+    -- lua, markdown, help, and query. Enable it for all filetypes that have
+    -- an installed parser.
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        if not vim.b[args.buf].ts_highlight then
+          pcall(vim.treesitter.start, args.buf)
+        end
+      end,
+    })
+
+    require("nvim-treesitter").setup({
       ensure_installed = {
         -- mandatory parsers
         "c",
@@ -87,83 +86,16 @@ return {
         "css",
         "html",
         "javascript",
-        "latex",
         "norg",
         "scss",
         "svelte",
         "tsx",
         "typst",
         "vue",
+        -- custom parsers
+        "jjdescription",
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-
-      -- List of parsers to ignore installing (or "all")
-      ignore_install = { "" },
-
-      autopairs = { enable = true },
-      autotag = { enable = true },
-      highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "help" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
-      },
-      indent = {
-        enable = true,
-        disable = {
-          "lua",
-          "python",
-          "yaml",
-        },
-      },
-      matchup = { enable = true },
-      -- textobjects = {
-      -- select = {
-      --   enable = true,
-      --
-      --   -- Automatically jump forward to textobj, similar to targets.vim
-      --   lookahead = true,
-      --
-      --   keymaps = {
-      --     -- You can use the capture groups defined in textobjects.scm
-      --     ["af"] = "@function.outer",
-      --     ["if"] = "@function.inner",
-      --     ["ac"] = "@class.outer",
-      --     -- You can optionally set descriptions to the mappings (used in the desc parameter of
-      --     -- nvim_buf_set_keymap) which plugins like which-key display
-      --     ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      --     -- You can also use captures from other query groups like `locals.scm`
-      --     ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-      --   },
-      --   -- You can choose the select mode (default is charwise 'v')
-      --   --
-      --   -- Can also be a function which gets passed a table with the keys
-      --   -- * query_string: eg '@function.inner'
-      --   -- * method: eg 'v' or 'o'
-      --   -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      --   -- mapping query_strings to modes.
-      --   selection_modes = {
-      --     ["@parameter.outer"] = "v", -- charwise
-      --     ["@function.outer"] = "V", -- linewise
-      --     ["@class.outer"] = "<c-v>", -- blockwise
-      --   },
-      --   -- If you set this to `true` (default is `false`) then any textobject is
-      --   -- extended to include preceding or succeeding whitespace. Succeeding
-      --   -- whitespace has priority in order to act similarly to eg the built-in
-      --   -- `ap`.
-      --   --
-      --   -- Can also be a function which gets passed a table with the keys
-      --   -- * query_string: eg '@function.inner'
-      --   -- * selection_mode: eg 'v'
-      --   -- and should return true or false
-      --   include_surrounding_whitespace = true,
-      -- },
-      -- },
     })
   end,
 }
